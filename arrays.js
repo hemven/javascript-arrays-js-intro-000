f1a66ff4c359741db29c771326dcf8ad3e3d8779
Hema Venkata <hvenkata@live.com>
2019-06-13 21:04:08 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+//  var array = [...element, array]
+//  var element = 'foo'
+//  var newArray = array.unshift(element);
+  const newArray = [element, ...array]
+  //var newArray
+  //return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var array = array.unshift(element)
+}
