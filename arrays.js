0ca1d5378efe4e78b435a9baf156b0b7eff86f74
Hema Venkata <hvenkata@live.com>
2019-06-13 21:19:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,16 +1,21 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-//  var array = [...element, array]
-//  var element = 'foo'
-//  var newArray = array.unshift(element);
-//  const newArray = [element, ...array]
-//  var newArray = array.unshift(element)
   var newArray = [element, ...array]
   return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.shift(element)
   return array
 }
